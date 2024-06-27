f3af4b4e669f9f9b7cf8f427ac6ac4c993877f3e
Chris Najman <christopher.najman@gmail.com>
2024-06-27 12:12:16 +0100

    Removed extraneous code from require-auth.js

--- src/utils/require-auth.js
+++ src/utils/require-auth.js
@@ -7,7 +7,6 @@ const basename = "/protected-routes"
 export async function requireAuth(request) {
   let pathname = new URL(request.url).pathname
 
-  // Remove the basename from the pathname
   if (pathname.startsWith(basename)) {
     pathname = pathname.slice(basename.length)
   }
@@ -24,22 +23,3 @@ export async function requireAuth(request) {
 
   return null
 }
-
-// import { redirect } from "react-router-dom"
-// import { LOGGEDIN_KEY } from "./localStorageKeys"
-
-// export async function requireAuth(request) {
-//   const pathname = new URL(request.url).pathname
-
-//   const isLoggedIn = localStorage.getItem(LOGGEDIN_KEY)
-
-//   if (!isLoggedIn) {
-//     const response = redirect(
-//       `/login?message=You must log in first.&redirectTo=${pathname}`
-//     )
-//     Object.defineProperty(response, "body", { value: true })
-//     throw response
-//   }
-
-//   return null
-// }
